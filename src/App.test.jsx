6028a727f4e6e5a955fe296daef10ab50132d825
asilvaolabi <asilva@olabi.org.br>
2024-04-08 22:25:33 -0300

    first commit

---
+++ src/App.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("Button starts with correct color", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", {name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
+});
+
+test("Button starts with correct text", () => {
+  render(<App />);
+  
+});
+
+test("Button has correct color after click", () => {
+  render(<App />);
+  
+});
+
+test("Button has correct text after click", () => {
+  render(<App />);
+  
+});
